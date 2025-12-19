7602bd7301d217a142b539335c70b4bfecb0b7fa
manasamantripragada-oss <manasa.mantripragada@gmail.com>
2025-12-19 20:30:10 +0530

    Added files relevant to the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Brain Tasks App</h1>
+      <p>Application is running successfully 🚀</p>
+    </div>
+  );
+}
+
+export default App;
