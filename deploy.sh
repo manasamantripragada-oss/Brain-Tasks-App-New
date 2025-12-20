d6ffa2192ac935dfeed18f01dbf8f47b704db24e
manasamantripragada-oss <manasa.mantripragada@gmail.com>
2025-12-20 14:00:56 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
 
-echo "Deploying to EKS..."
-
+echo "Updating kubeconfig..."
 aws eks update-kubeconfig \
   --region ap-south-1 \
   --name brain-tasks-cluster
 
+echo "Deploying application to EKS..."
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
+
+echo "Deployment successful"
